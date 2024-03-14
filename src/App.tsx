1f494309a58944d047a58f379685daa86a32a6a3
Maryam Nasir <maryamnasir555@yahoo.com>
2024-03-14 18:00:26 +0500

    Add routes for SignIn, SignUp and Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import './globals.css'
+import { Routes, Route } from "react-router-dom";
+import "./globals.css";
+
+import SignInForm from "./_auth/forms/SignInForm";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import { Home } from "./_root/pages";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
 
-export default App
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
+
+export default App;
